803f4b9880d12922c13b22dabfc4c6096028b9b1
afrizzal <afrizzal@gmail.com>
2021-05-30 17:34:10 +0700

    Add middleware on dashboard

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -9,6 +9,9 @@ use Validator;
 
 class UserController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/dashboard/DashboardController.php
+++ app/Http/Controllers/dashboard/DashboardController.php
@@ -23,9 +23,9 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($id)
     {
-        //
+        
     }
 
     /**
